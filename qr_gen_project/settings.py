a3997f839b8b8d8aa4b761495860a67c2f6b0696
Wisdom Emmanuel <simplenick01@gmail.com>
2022-08-19 22:07:25 +0100

    .

--- qr_gen_project/settings.py
+++ qr_gen_project/settings.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+import dotenv
+
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
